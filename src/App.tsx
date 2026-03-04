d97b6648658e74cd8baaebe3d0cabf993f592be1
harukee-dev <hdjemsmc@gmail.com>
2026-03-04 22:35:58 +0300

    сделан header со стилизацией

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { AreaChartComponent } from './Components/Charts/AreaChart/AreaChart'
 import { BarChartComponent } from './Components/Charts/BarChart/BarChart'
 import { LineChartComponent } from './Components/Charts/LineChart/LineChart'
 import { PieChartComponent } from './Components/Charts/PieChart/PieChart'
+import { Header } from './Components/Header/Header'
 
 function App() {
   return (
-    <div style={{ backgroundColor: '#1F1F1F' }}>
-      {/* <h1>Hello world!</h1>
+    <BrowserRouter>
+      <div style={{ backgroundColor: '#131313' }}>
+        {/* <h1>Hello world!</h1>
       <AreaChartComponent />
       <BarChartComponent />
       <LineChartComponent /> */}
-      <PieChartComponent />
-    </div>
+        <Header />
+        <PieChartComponent />
+      </div>
+    </BrowserRouter>
   )
 }
 
